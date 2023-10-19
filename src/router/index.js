10fb28c81c3d6bd1b3e0f6881c035cd162360675
Phung-B2005729 <phungb2005729@student.ctu.edu.vn>
2023-10-19 22:45:57 +0700

    Buoc 5 cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,12 @@ import ContactBook from "@/views/ContactBook.vue";
             name: "contactbook",
             component: ContactBook,
         },
+        {
+            path: "/:pathMatch(.*)*",
+            name: "notfound",
+            component: () => import("@/views/NotFound.vue"),
+        },
+
     ];
     const router = createRouter({
         history: createWebHistory(import.meta.env.BASE_URL),
